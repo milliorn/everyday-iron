888ea5619cfd490fab71a144ed67d1c5827b9611
Philip Milliorn <scottmilliorn@outlook.com>
2021-02-15 18:17:51 -0800

    npm start

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,8 +14,14 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
-    `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-transformer-sharp`,
+      options: {
+        // The option defaults to true
+        checkSupportedExtensions: false,
+      },
+    },
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
