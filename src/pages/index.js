8116bf731b74dc018ee8f92d939c8d738ab3f5f2
Philip Milliorn <scottmilliorn@outlook.com>
2021-02-16 19:11:04 -0800

    bold text

--- src/pages/index.js
+++ src/pages/index.js
@@ -220,7 +220,7 @@ export default class IndexPage extends React.Component {
               <div className="col-lg-8 text-center">
                 <h2 className="mt-0">Owned and Operated by U.S. ARMY Veteran</h2>
                 <hr className="divider my-4" />
-                <p className="text-muted mb-5">
+                <p className="text-bold mb-5">
                   Ready to start your fitness journey with us? Give us a call or send us an
                   email and we will get back to you as soon as possible!
                 </p>
@@ -228,7 +228,7 @@ export default class IndexPage extends React.Component {
             </div>
             <div className="row">
               <div className="col-lg-4 ml-auto text-center mb-5 mb-lg-0">
-                <i className="fas fa-phone fa-3x mb-3 text-muted"></i>
+                <i className="fas fa-phone fa-3x mb-3 text-bold"></i>
                 <div>+1 (202) 555-0149</div>
               </div>
               <div className="col-lg-4 mr-auto text-center">
