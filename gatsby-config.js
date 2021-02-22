c326bfb9461a14e35a7485dd1db7ad4e455cdfad
Philip Milliorn <scottmilliorn@outlook.com>
2021-02-21 18:06:23 -0800

    removed purgecss

--- gatsby-config.js
+++ gatsby-config.js
@@ -47,17 +47,6 @@ module.exports = {
         },
       },
     },
-    {
-      resolve: `gatsby-plugin-purgecss`,
-      options: {
-        printRejected: true, // Print removed selectors and processed file names
-        // develop: true, // Enable while using `gatsby develop`
-        // tailwind: true, // Enable tailwindcss support
-        // whitelist: ['whitelist'], // Don't remove this selector
-        ignore: ['header.js', '_navbar.scss/'], // Ignore files/folders
-        // purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
-      }
-    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     `gatsby-plugin-offline`,
