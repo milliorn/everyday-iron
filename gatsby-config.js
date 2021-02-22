cccfc5644069a459158631e900032b92614a7f8c
Philip Milliorn <scottmilliorn@outlook.com>
2021-02-21 18:23:32 -0800

    typo

--- gatsby-config.js
+++ gatsby-config.js
@@ -47,12 +47,14 @@ module.exports = {
         },
       },
     },
+    {
     resolve: `gatsby-plugin-canonical-urls`,
-    options: {
-      siteUrl: `https://everyday-iron.netlify.app`,
-    },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    `gatsby-plugin-offline`,
+      options: {
+        siteUrl: `https://everyday-iron.netlify.app`,
+      }
+    }
+      // this (optional) plugin enables Progressive Web App + Offline functionality
+      // To learn more, visit: https://gatsby.dev/offline
+      `gatsby-plugin-offline`,
   ],
 }
