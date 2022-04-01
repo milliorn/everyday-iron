6e2acbaf53468d22d5e115cbb111fd45e5321731
Scott Milliorn <scottmilliorn@gmail.com>
2022-03-31 19:54:00 -0700

    hero

--- project/src/App.js
+++ project/src/App.js
@@ -1,168 +1,21 @@
-import { Transition } from "@headlessui/react";
-import React, { useState } from "react";
+import React from "react";
 import './App.css';
-import { GiRhinocerosHorn } from "react-icons/gi";
-
-function Nav() {
-  const [ isOpen, setIsOpen ] = useState(false);
-  return (
-    <div>
-      <nav className="bg-gray-800">
-        <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
-          <div className="flex items-center justify-between h-16">
-            <div className="flex items-center">
-              <div className="flex-shrink-0">
-                <GiRhinocerosHorn className="w-8 h-8 text-white" alt="rhino" />
-              </div>
-              <div className="hidden md:block">
-                <div className="flex items-baseline ml-10 space-x-4">
-                  <a
-                    href="/"
-                    className="px-3 py-2 text-sm font-medium text-white rounded-md hover:bg-gray-700"
-                  >
-                    Dashboard
-                  </a>
-
-                  <a
-                    href="/"
-                    className="px-3 py-2 text-sm font-medium text-gray-300 rounded-md hover:bg-gray-700 hover:text-white"
-                  >
-                    Team
-                  </a>
-
-                  <a
-                    href="/"
-                    className="px-3 py-2 text-sm font-medium text-gray-300 rounded-md hover:bg-gray-700 hover:text-white"
-                  >
-                    Projects
-                  </a>
-
-                  <a
-                    href="/"
-                    className="px-3 py-2 text-sm font-medium text-gray-300 rounded-md hover:bg-gray-700 hover:text-white"
-                  >
-                    Calendar
-                  </a>
-
-                  <a
-                    href="/"
-                    className="px-3 py-2 text-sm font-medium text-gray-300 rounded-md hover:bg-gray-700 hover:text-white"
-                  >
-                    Reports
-                  </a>
-                </div>
-              </div>
-            </div>
-            <div className="flex -mr-2 md:hidden">
-              <button
-                onClick={() => setIsOpen(!isOpen)}
-                type="button"
-                className="inline-flex items-center justify-center p-2 text-gray-400 bg-gray-900 rounded-md hover:text-white hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
-                aria-controls="mobile-menu"
-                aria-expanded="false"
-              >
-                <span className="sr-only">Open main menu</span>
-                {!isOpen ? (
-                  <svg
-                    className="block w-6 h-6"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    aria-hidden="true"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M4 6h16M4 12h16M4 18h16"
-                    />
-                  </svg>
-                ) : (
-                  <svg
-                    className="block w-6 h-6"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    aria-hidden="true"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M6 18L18 6M6 6l12 12"
-                    />
-                  </svg>
-                )}
-              </button>
-            </div>
-          </div>
-        </div>
-        <Transition
-          show={isOpen}
-          enter="transition ease-out duration-100 transform"
-          enterFrom="opacity-0 scale-95"
-          enterTo="opacity-100 scale-100"
-          leave="transition ease-in duration-75 transform"
-          leaveFrom="opacity-100 scale-100"
-          leaveTo="opacity-0 scale-95"
-        >
-          {(ref) => (
-            <div className="md:hidden" id="mobile-menu">
-              <div ref={ref} className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
-                <a
-                  href="/"
-                  className="block px-3 py-2 text-base font-medium text-white rounded-md hover:bg-gray-700"
-                >
-                  Dashboard
-                </a>
-
-                <a
-                  href="/"
-                  className="block px-3 py-2 text-base font-medium text-gray-300 rounded-md hover:bg-gray-700 hover:text-white"
-                >
-                  Team
-                </a>
-
-                <a
-                  href="/"
-                  className="block px-3 py-2 text-base font-medium text-gray-300 rounded-md hover:bg-gray-700 hover:text-white"
-                >
-                  Projects
-                </a>
-
-                <a
-                  href="/"
-                  className="block px-3 py-2 text-base font-medium text-gray-300 rounded-md hover:bg-gray-700 hover:text-white"
-                >
-                  Calendar
-                </a>
-
-                <a
-                  href="/"
-                  className="block px-3 py-2 text-base font-medium text-gray-300 rounded-md hover:bg-gray-700 hover:text-white"
-                >
-                  Reports
-                </a>
-              </div>
-            </div>
-          )}
-        </Transition>
-      </nav>
-
-    </div>
-  );
-}
+import HeroImage from "./images/hero.jpg";
 
 function App() {
+  const url = "https://www.pluralsight.com/guides/how-to-render-%22a%22-with-optional-href-in-react";
+
   return (
-    <div className="App">
-      <Nav />
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+    <div>
+      <div className="relative w-full max-h-fit">
+        <img src={HeroImage} alt="" className="w-screen h-100" />
+        <button className="absolute px-6 py-3 mx-auto my-0 text-xl font-bold text-center text-white bg-green-500 border-none rounded-full cursor-pointer sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl top-3/4 left-1/2">
+          <a href={url}>Learn More</a>
+        </button>
+      </div>
+
     </div>
+
   );
 }
 
