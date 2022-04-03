8a3b46de7658c00fb892ad9324ddebaab8741a67
Scott Milliorn <scottmilliorn@gmail.com>
2022-04-02 19:59:44 -0700

    top bottom (#89)

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -1,21 +1,48 @@
-import React, { useRef } from "react";
-import "./App.css";
-import HeroImage from "./images/hero.jpg";
-import SoldierImage from "./images/soldier.jpg";
-import Apf from "./images/apf.jpg";
-
+import { useEffect, useRef, useState } from "react";
+import { AiOutlineMail } from "react-icons/ai";
 import {
   BsFacebook,
-  BsInstagram,
-  BsFillTelephoneFill,
   BsFillArrowDownCircleFill,
+  BsFillArrowUpCircleFill,
+  BsFillTelephoneFill,
+  BsInstagram,
 } from "react-icons/bs";
-import { SiGooglemaps, SiCashapp } from "react-icons/si";
-import { AiOutlineMail } from "react-icons/ai";
+import { SiCashapp, SiGooglemaps } from "react-icons/si";
+import "./App.css";
+import Apf from "./images/apf.jpg";
+import HeroImage from "./images/hero.jpg";
+import SoldierImage from "./images/soldier.jpg";
 
 function App() {
   const soldierImage = useRef();
   const learnMoreButton = useRef();
+
+  const [isVisible, setIsVisible] = useState(false);
+
+  const toggleVisibility = () => {
+    if (window.pageYOffset > 30) {
+      setIsVisible(true);
+    } else {
+      setIsVisible(false);
+    }
+  };
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", toggleVisibility);
+
+    return () => {
+      window.removeEventListener("scroll", toggleVisibility);
+    };
+  }, []);
+
+  const classNames = (...classes) => classes.filter(Boolean).join(" ");
 
   function jumpToSoldier() {
     soldierImage.current.scrollIntoView({ behavior: "smooth" });
@@ -38,6 +65,22 @@ function App() {
 
   return (
     <div className="mx-auto bg-black 2xl:container">
+      <div className="fixed z-10 top-8 left-8 lg:top-11 lg:left-11 xl:top-12 xl:left-12">
+        <button
+          type="button"
+          onClick={scrollToTop}
+          className={classNames(
+            isVisible ? "opacity-100" : "opacity-0",
+            'bg-white rounded-full cursor-pointer drop-shadow-2xl top-3/4 left-1/2 animate-pulse"'
+          )}
+        >
+          <BsFillArrowUpCircleFill
+            className="text-3xl text-green-900 sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl"
+            aria-hidden="true"
+          />
+        </button>
+      </div>
+
       <div className="relative w-full max-h-fit">
         <img
           src={HeroImage}
