39bba47cf20af6483279b9b7737155c5efc854a7
Scott Milliorn <scottmilliorn@gmail.com>
2022-04-06 21:31:45 -0700

    moved files (#104)

---
+++ src/components/About.jsx
@@ -0,0 +1,21 @@
+
+export function About(props) {
+  return (
+    <section className="relative w-full max-h-fit" ref={props.aboutImage}>
+      <img
+        src={props.src}
+        alt={props.alt}
+        className="w-screen h-100 brightness-105 "
+      />
+      <p className="absolute mx-auto my-0 text-sm font-semibold text-center text-green-50 backdrop-blur-sm drop-shadow-2xl sm:text-lg md:text-xl top-2/4 left-1/2">
+        {props.paragraph}
+      </p>
+      <button
+        onClick={props.onClick}
+        className="absolute px-3 py-1 text-xs capitalize bg-green-900 rounded-full cursor-pointer sm:text-base lg:text-lg xl:text-2xl text-green-50 drop-shadow-2xl top-3/4 left-1/2 "
+      >
+        <span>{props.span}</span>
+      </button>
+    </section>
+  );
+}
---
+++ src/data.js
@@ -0,0 +1,36 @@
+import { AiOutlineMail } from "react-icons/ai";
+import { BsFacebook, BsFillTelephoneFill, BsInstagram } from "react-icons/bs";
+import { SiCashapp, SiGooglemaps } from "react-icons/si";
+
+export const data = [
+  {
+    alt: "cash-app",
+    href: "https://cash.app/$RhinoLopez",
+    icon: <SiCashapp />
+  },
+  {
+    alt: "facebook",
+    href: "https://www.facebook.com/rhinosEI",
+    icon: <BsFacebook />
+  },
+  {
+    alt: "instagram",
+    href: "https://www.instagram.com/everyday_iron/",
+    icon: <BsInstagram />
+  },
+  {
+    alt: "google maps",
+    href: "https://goo.gl/maps/abEtXLCVTWeupqKD8",
+    icon: <SiGooglemaps />
+  },
+  {
+    alt: "phone number",
+    href: "tel:+1-559-577-8679",
+    icon: <BsFillTelephoneFill />
+  },
+  {
+    alt: "email",
+    href: "mailto:everydayiron8489@gmail.com",
+    icon: <AiOutlineMail />
+  }
+]
