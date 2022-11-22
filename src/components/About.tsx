8c457ade75bd6f658e4941295a6fe035c5dffc6b
milliorn <scottmilliorn@gmail.com>
2022-11-22 01:51:07 -0800

    fix background contrast issue, suspense

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -49,7 +49,7 @@ function AboutTextContainer(): JSX.Element {
   return (
     <div className="w-full md:w-5/12 ml-auto mr-auto px-4" data-aos="fade">
       <div className="md:pr-12">
-        <h2 className="sm:text-3xl 2xl:text-4xl text-xl uppercase font-bold font-serif	text-red-600 mt-4 sm:mt-0 text-center md:text-left">
+        <h2 className="sm:text-3xl 2xl:text-4xl text-xl uppercase font-extrabold font-serif	text-red-600 mt-4 sm:mt-0 text-center md:text-left">
           Rhino ATHLETIC Performance
         </h2>
         <AboutList />
