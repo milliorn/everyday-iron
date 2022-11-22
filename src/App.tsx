23876501cdfd86e58332dc0d86a031ea34ebd3c7
milliorn <scottmilliorn@gmail.com>
2022-11-21 18:28:37 -0800

    duration

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ export default function App(): JSX.Element {
   useEffect(() => {
     Aos.init({
       /** https://github.com/michalsnik/aos#predefined-options */
-      duration: 2000, // values from 0 to 3000, with step 50ms
+      duration: 1000, // values from 0 to 3000, with step 50ms
       easing: "ease", // default easing for AOS animations
       once: false, // whether animation should happen only once - while scrolling down
     });
