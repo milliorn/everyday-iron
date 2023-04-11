90d44b2fa3e8349577c97e07d27abb6eaf309b7e
milliorn <scottmilliorn@gmail.com>
2023-04-10 23:23:31 -0700

    comments

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,7 @@ import { Disclosure } from "@headlessui/react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 
 const navigation = [
-  { name: "Home", href: "#home", current: false, target: "" },
+  { name: "Home", href: "#home", current: true, target: "" },
   { name: "About", href: "#about", current: false, target: "" },
   { name: "Trainers", href: "#trainer", current: false, target: "" },
   { name: "Contact", href: "#contact", current: false, target: "" },
@@ -13,11 +13,12 @@ function classNames(...classes: string[]): string {
   return classes.filter(Boolean).join(" ");
 }
 
-export default function Navbar() {
+export default function Navbar(): JSX.Element {
   return (
     <Disclosure as="nav" className="bg-neutral-900 fixed top-0 w-full z-10">
       {({ open }) => (
         <>
+          {/* Desktop navigation */}
           <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
             <div className="relative flex h-16 items-center justify-between">
               <div className="absolute inset-y-0 right-0 flex items-center sm:hidden">
@@ -35,12 +36,14 @@ export default function Navbar() {
               <div className="flex flex-1 items-center justify-center sm:items-stretch">
                 <div className="hidden sm:ml-6 sm:block ml-auto">
                   <div className="flex space-x-4">
+                    {/* Map through navigation array and create links */}
                     {navigation.map((item) => (
                       <a
                         target={item.target}
                         key={item.name}
                         href={item.href}
                         className={classNames(
+                          // Set background color and text color based on "current" property (disabled)
                           item.current
                             ? "bg-neutral-900 text-white"
                             : "text-gray-300 hover:bg-neutral-700 hover:text-white",
@@ -58,6 +61,7 @@ export default function Navbar() {
             </div>
           </div>
 
+          {/* Mobile navigation */}
           <Disclosure.Panel className="sm:hidden">
             <div className="space-y-1 px-2 pb-3 pt-2">
               {navigation.map((item) => (
