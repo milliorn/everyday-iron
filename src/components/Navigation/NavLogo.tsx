5d2a7e63fb5389e697fa7f11f56bc909962a39c1
milliorn <scottmilliorn@gmail.com>
2023-04-12 13:29:50 -0700

    NavLogo

---
+++ src/components/Navigation/NavLogo.tsx
@@ -0,0 +1,10 @@
+import { Img } from "react-image";
+
+export function NavLogo(): JSX.Element {
+  return (
+    <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+      {/* Display either an X icon or a menu icon depending on the value of "open" */}
+      test
+    </div>
+  );
+}
