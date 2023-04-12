4847697596045d0ca723e3de922afa9b8ff7777c
milliorn <scottmilliorn@gmail.com>
2023-04-11 21:35:41 -0700

    nav button

--- src/components/Events/Event.tsx
+++ src/components/Events/Event.tsx
@@ -3,7 +3,7 @@ import { EventWrapper } from "./EventWrapper";
 /** app about component */
 export default function Event(): JSX.Element {
   return (
-    <section id="about" className="relative py-20 bg-black text-white">
+    <section id="event" className="relative py-20 bg-black text-white">
       <div className="container mx-auto px-4">
         <EventWrapper />
       </div>
