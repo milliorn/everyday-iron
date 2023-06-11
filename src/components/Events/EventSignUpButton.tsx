5c27f2a4add577c92c0787308f9a7e7dfa3f1dfb
Scott Milliorn <scottmilliorn@gmail.com>
2023-06-10 19:13:48 -0700

    update typescript, types, events, forms, img (#197)
    Co-authored-by: milliorn <scottmilliorn@gmai.com>

--- src/components/Events/EventSignUpButton.tsx
+++ src/components/Events/EventSignUpButton.tsx
@@ -4,7 +4,7 @@ export function EventSignUpButton(): JSX.Element {
     <div className="text-center">
       {/** https://stackoverflow.com/a/9721865/11986604 */}
       <a
-        href="https://form.jotform.com/230717412394151"
+        href="https://form.jotform.com/231541154185148"
         target="_blank"
         className="bg-transparent hover:bg-red-600 text-red-600 font-semibold hover:text-white p-4 border border-red-600 hover:border-transparent rounded inline-block my-8 cursor-pointer 2xl:text-2xl capitalize"
         rel="noreferrer"
